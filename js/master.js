6c8857d1d86a18211f04cdf42fc4f9693ccdb94a
bb837 <43794585+bb837@users.noreply.github.com>
2019-03-05 03:21:34 -0500

    update to content
    css cleaned
    js cleaned (alg refined)
    html cleaned
    
    js to do list, at bottom

--- js/master.js
+++ js/master.js
@@ -1,4 +1,4 @@
-//       OBJECTS
+//       OBJECTS        //////////////////////////////////////////////////////
 
 const Ari = {
   value: 0,
@@ -17,7 +17,7 @@ const Tau = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Driven, Fixed, Trustworthy',
-  description: 'some vague fru fru text 1',
+  description: "Have you ever met someone who was hardworking, persistent and even a little bit stubborn? Chances are, this person was a Taurus. Among the traits of a Taurus is their built-in energy conservation program. Taurus energy has two speeds: relaxed and contented or hyped-up and ready to charge like a Bull. A Taurus will patiently assess whether something is worth the investment of their time and resources. But until then, slow and steady wins the Taurus race. This sign encourages us to break our work into simple steps then take daily action. Taurus is the sign of the persistent provider who loves life’s earthly pleasures. As the zodiac’s second sign, Taurus craves a sense of security and stability.",
 }
 
 const Gem = {
@@ -27,7 +27,7 @@ const Gem = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Adaptable, Active, Communicative',
-  description: 'some vague fru fru text 2',
+  description: "The traits of a Gemini, the star sign ruled by the Twins, is their desire for synergistic communication. Gemini has strong opinions and personalities and they often need to be the center of attention. They are highly versatile love a good argument, but will often butt heads with those whose opinions differ from their own. It takes a lot to hold an Gemini’s attention span—though once they find their passion they go in all the way! Gemini is the sign of kindred spirit energy. As the zodiac’s third sign, Gemini loves dynamic duos and is always seeking their other half.",
 }
 
 const Can = {
@@ -37,7 +37,7 @@ const Can = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Nuturing, Receptive, Supportive',
-  description: 'some vague fru fru text 3',
+  description: "The Cancer has their inherent need to serve as a caregiver. A Cancer’s familial ties are strong, with emphasis on planting deep roots and feathering their nests. They love to encourage their loved ones to connect to their feelings, but can sometimes too strongly connect to their own! Ruled by the moon, a Cancer is known to get moody at times, but is also just sentimental and nostalgic by nature. Cancer is the sign of the feminine and the “mother” archetype. As the zodiac’s fourth sign, a Cancer’s love runs deep.",
 }
 
 const Leo = {
@@ -47,7 +47,7 @@ const Leo = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Active, Playful, Warm',
-  description: 'some vague fru fru text 4',
+  description: "A Leo has a strong personality and a desire to take the stage (both literally and proverbially). They are proud and will wear their heart on their sleeve, but can often get swept away in personal theatrics. Yet, a Leo’s flair for powerful leadership can also let everyone have their spot in the sunshine. Leo is the sign of the hopeless romantic. As the zodiac’s fifth sign, Leo loves to be the center of attention. Positive Leo astrology traits include being generous, loyal, nakedly honest and entertaining. They’re not afraid to let their true essence shine in the Sun, which is also their planetary ruler. Leo energy leaves others feeling awakened and revitalized in an empowering way.",
 }
 
 const Vir = {
@@ -57,7 +57,7 @@ const Vir = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Humble, Logical, Organized',
-  description: 'some vague fru fru text 5',
+  description: "A Virgo is very opinionated and they despise laziness. They are highly productive and love analyzing a situation, but can often get stuck in analysis-paralysis as they nit-pick at all the details. Ruled by communication planet Mercury, a Virgo is an expert conversationalist. Virgo is the sign of the Virgin or Maiden, which is symbolic for a Virgo’s innocence or purity. As the zodiac’s sixth sign, Virgo loves anything streamlined and systematized. Virgo astrology traits include being dedicated, resourceful, helpful, hardworking and witty. They’re not afraid to speak their minds and voice their opinions! A Virgo can become anxious when they criticize themselves and others to the point of obsession. This is a sign that could benefit from the old adage, 'Nobody’s perfect.'",
 }
 
 const Lib = {
@@ -67,7 +67,7 @@ const Lib = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Charming, Easy-going',
-  description: 'some vague fru fru text 6',
+  description: "Have you ever met someone who was diplomatic, romantic, highly aesthetic and a little indulgent? This person is a Libra, the sign born between September 23 - October 22. Learning the Libra star sign personality and Libra traits can help you coexist in harmony with this zodiac sign, which can be peaceful and loving but also moody, stubborn and indecisive. The Libra is in the inherent need for partnership. Libras have strong personalities, but function the most optimally as a dynamic duo. They strive for justice and fairness, and they’re often better at starting than finishing. As Libra’s attention is often up in the clouds—but make no mistake, they are still avid listeners nonetheless! Libra is the sign of the trendsetter and idea innovator. As the zodiac’s seventh sign, Libra loves a complementary relationship.",
 }
 
 const Sco = {
@@ -77,7 +77,7 @@ const Sco = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Passionate, Driven, Emotional',
-  description: 'some vague fru fru text 7', 
+  description: "The Scorpio is protective and magnetic but also secretive, possessive and vengeful. Chief among the traits of a Scorpio is their fierce need for relationships built on trust that are meant to last. A Scorpio has intense eyes, a hawk-like gaze and smooth movements. Ruled by power-monger Pluto, a Scorpio loves to be in a position of authority. It takes a lot to earn a Scorpio’s trust—though once you do there’s a strong chance the bond will last a lifetime. Scorpio is the sign of the trendsetter and intense intimacy. As the zodiac’s eighth sign, a Scorpio is known for their bravery.", 
 }
 
 const Sag = {
@@ -87,7 +87,7 @@ const Sag = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Optimistic: Ambitious, Versatile',
-  description: 'some vague fru fru text 8',
+  description: "Have you ever met someone who was expansive, outspoken and even a little bit metaphysical? Chances are, this person was a Sagittarius. Learning the Sagittarius star sign personality and Sagittarius traits can help you coexist in harmony with this zodiac sign, which can be adventurous and a risk-taker, but can also have their head stuck in the clouds with their lofty goals. Chief among the traits of a Sagittarius is their fierce need for expansion. A Sagittarius has a strong personality and they never want to stop learning. They love to travel and explore, but can sometimes lose touch with reality. It takes a lot to keep a Sagittarius’ attention—though when they find their obsessions they go in all the way! Ruled by worldly Jupiter, Sagittarius is the sign of dreaming big. As the zodiac’s tenth sign, Sagittarius loves to learn anything new.",
 }
 
 const Cap = {
@@ -97,7 +97,7 @@ const Cap = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Patient, Responsible, Determined',
-  description: 'some vague fru fru text 9',
+  description: "The Capricorn is known for their fierce drive for success. Capricorns are strong-minded and mature, and they are always on their A-game. They are competitive and love a challenge, and love when a solid structure is put into place. Ruled by taskmaster Saturn, a Capricorn is incredibly disciplined and will always ensure that a project, no matter how big or small, is seen through to its completion. Capricorn is the sign of the slow and steady Mountain Goat. As the ruler of the zodiac’s tenth house, Capricorns can be highly career-focused. Capricorn is a leadership-oriented cardinal sign and a stabilizing earth sign. Many Capricorns secretly wish to be able to take care of everything themselves and can often work themselves to the bone as a result.",
 }
 
 const Aqu = {
@@ -107,7 +107,7 @@ const Aqu = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Intelligent, Humanistic, Inventive',
-  description: 'some vague fru fru text 10',
+  description: "Understanding Aquarius traits can help you coexist in harmony with this zodiac sign, who craves casual connections but can sometimes be emotionally detached. An Aquarius has the fierce need for rebellion. An Aquarius has a strong desire for freedom, but they also love to be surrounded by their vast network of friends. They are passionate advocates for their cutting edge ideas, but can sometimes come off as aloof. It takes a lot to hold an Aquarius’ attention span—though but when they find their obsessions they go in all the way! Aquarius is the sign of the trendsetter and trailblazer. As the zodiac’s eleventh sign, Aquarius loves creative collaborations and synergistic connections. Positive Aquarius astrology traits include communication, originality, open-mindedness, fairness, logical and inviting. They’re not afraid to speak up for what they believe. Aquarius becomes upset when they feel wronged in some way or when a friend or loved one has been hurt or offended. This friendship-focused sign has no tolerance for injustice!",
 }
 
 const Pis = {
@@ -117,40 +117,27 @@ const Pis = {
   image: 'TEMP',
   sound: 'TEMP',
   trait: 'Romantic, Creative, Sensitive',
-  description: 'some vague fru fru text 11',
+  description: "Understanding Aquarius traits can help you coexist in harmony with this zodiac sign, who craves casual connections but can sometimes be emotionally detached. An Aquarius has the fierce need for rebellion. An Aquarius has a strong desire for freedom, but they also love to be surrounded by their vast network of friends. They are passionate advocates for their cutting edge ideas, but can sometimes come off as aloof. It takes a lot to hold an Aquarius’ attention span—though but when they find their obsessions they go in all the way! Aquarius is the sign of the trendsetter and trailblazer. As the zodiac’s eleventh sign, Aquarius loves creative collaborations and synergistic connections. Positive Aquarius astrology traits include communication, originality, open-mindedness, fairness, logical and inviting. They’re not afraid to speak up for what they believe. Aquarius becomes upset when they feel wronged in some way or when a friend or loved one has been hurt or offended. This friendship-focused sign has no tolerance for injustice!",
 }
 
 const getSign = [Ari,Tau,Gem,Can,Leo,Vir,Lib,Sco,Sag,Cap,Aqu,Pis];
 
-//     INFO
+//     INFO     ////////////////////////////////////////////////////////
 
-// have the doc create the text on selected index
-
-//     CAROUSEL BOI
+    // CAROUSEL ////////////////////////////////////////////////////////
 
 function OpenNav() {
   document.getElementById('selected_info').style.height = '30vh';
   selectedIndexNormal
 }
 
-document.getElementById('submit_car').addEventListener("click",OpenNav, false);
+// document.getElementById('submit_car').addEventListener("click",OpenNav, false);
 
-
-
-//     FORM BOI
-
-// var whichMonth = document.getElementById('sel_month').value; // get the value attribute 
-
-// // console.log(whichMonth);
-
-// var whichDayOfMonth = document.getElementById('birth_day').value; // get number value from 1-31
-
-// console.log(whichDayOfMonth);
+  // FORM BOI  /////////////////////////////////////////////////////////
 
 var submitBtn = document.getElementById('submit');
+
 // console.log(submitBtn);
-
-
 
 submitBtn.addEventListener('click', function() {
   var whichMonth = document.getElementById('sel_month').value; // get the value attribute 
@@ -158,12 +145,11 @@ submitBtn.addEventListener('click', function() {
 // console.log(whichMonth);
 
 var whichDayOfMonth = document.getElementById('birth_day').value; // get number value from 1-31
-
   getSignData(whichMonth, whichDayOfMonth);
-
-selectedIndexNormal = eval(getSignData(whichMonth, whichDayOfMonth)).value;
+  selectedIndexNormal = eval(getSignData(whichMonth, whichDayOfMonth)).value;
 
   // console.log('click');
+
   OpenNav();
 
   document.getElementById('zodiac_name').innerText = getSign[selectedIndexNormal].name;
@@ -171,17 +157,7 @@ selectedIndexNormal = eval(getSignData(whichMonth, whichDayOfMonth)).value;
   document.getElementById('vague_fru_text').innerText = getSign[selectedIndexNormal].description;
 })
 
-// function Submit () {
-//   getSignData(whichMonth, whichDayOfMonth);
-//  document.getElementById('submit').addEventListener('click', function() {
-//   console.log('shit works');
-//  })
-//   console.log(whichMonth, whichDayOfMonth);
-
-//   return AstroSign
-// }
-
-//     ZODIAC DATES
+//     ZODIAC DATES    ///////////////////////////////////////////////////////
 
 function getSignData (whichMonth, whichDayOfMonth) {
 
@@ -216,22 +192,20 @@ if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayO
   }
 
 if ((whichMonth == 1|| whichMonth == 3|| whichMonth == 5|| whichMonth == 7|| whichMonth == 8||
-  whichMonth == 10|| whichMonth == 12) && whichDayOfMonth > 31) {
+  whichMonth == 10|| whichMonth == 12) && (whichDayOfMonth < 1 || whichDayOfMonth > 31)) {
 
     AstroSign = 'error';
   
  } else if ((whichMonth == 9|| whichMonth == 4|| whichMonth == 6|| whichMonth == 11)
-      && whichDayOfMonth > 30) {
+ && (whichDayOfMonth < 1 || whichDayOfMonth > 30)) {
 
    AstroSign = 'error';
       
- } else if  ((whichMonth == 2)
-      && whichDayOfMonth > 29) {
+  } else if  ((whichMonth == 2)
+  && (whichDayOfMonth < 1 || whichDayOfMonth > 29)) {
       
         AstroSign = 'error';
   }
-
-  //    NEGATIVES AND 0 ARE WORKING.   ITS A NO NO
 
   console.log(AstroSign);
 
@@ -248,7 +222,7 @@ function rotateCarousel() {
   var angle = selectedIndex / cellCount * -360;
   carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
 
-  console.log(angle);
+  // console.log(angle);
 }
 
 var prevButton = document.querySelector('.previous-button');
@@ -260,7 +234,7 @@ prevButton.addEventListener( 'click', function() {
   }
   rotateCarousel();
   // console.log(selectedIndex);
-  // console.log(selectedIndexNormal);
+  console.log(selectedIndexNormal);
 });
 
 var nextButton = document.querySelector('.next-button');
@@ -272,8 +246,12 @@ nextButton.addEventListener( 'click', function() {
   }
   rotateCarousel();
   // console.log(selectedIndex);
-  // console.log(selectedIndexNormal);
+  console.log(selectedIndexNormal);
 });
 
-// if selectedIndex = 1-12 then apply this code
-// add elements   call from object name and date
+// MAKE ERROR SIGN (SEE IF ERROR IS ENOUGH)
+// ADD EVENT LISTENERS TO THE IMGS IN CAROSEL TO SHOW FRU FRU TEXT ON CLICK
+// FIGURE OUT HOW TO SHOW COMPATABILITY
+//HAVE A BUTTON CREATE UNDER FRU FRU TEXT TO REDIRECT ANGLE/CELL OF CAROUSEL
+// AND AUTOMATICALLY SHOW THE FRU FRU TEXT OF THE MATCH 
+//ADD RESTART BUTTON TO START OVER
